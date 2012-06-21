12ffa72c95c4301d01d7a56eece9300d84de1132
Areerat Wongsila <iii_crazilyzzz@hotmail.com>
2012-06-21 12:46:21 +0700

    เพิ่ม "Programming is fun"

--- src/th/ac/nu/students/u54347804/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54347804/compro/ch01/HelloWorld.java
@@ -3,5 +3,6 @@ package th.ac.nu.students.u54347804.compro.ch01;
 public class HelloWorld {
   public static void main(String[] args) {
        System.out.println("Hello,World");
+       System.out.println("Programming is fun");
     }
 }
