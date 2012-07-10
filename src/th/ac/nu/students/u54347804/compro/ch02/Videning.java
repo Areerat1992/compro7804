ed731a173f36e2c6f409ed312eb887e74110e767
Areerat Wongsila <iii_crazilyzzz@hotmail.com>
2012-07-10 15:44:53 +0700

    เพิ่ม Chapter2

---
+++ src/th/ac/nu/students/u54347804/compro/ch02/Videning.java
@@ -0,0 +1,10 @@
+package th.ac.nu.students.u54347804.compro.ch02;
+
+public class Videning {
+
+    public static void main(String[] args) {
+        int i = 1234567890;
+        float f = i;
+        System.out.println(i - (int) f);
+    }
+}
